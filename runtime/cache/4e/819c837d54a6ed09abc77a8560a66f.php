1197e331931bd55e93d4c588fd1154b7cf561f81
itdjmeng <15991780596@163.com>
2018-07-11 12:06:09 +0800

    删除缓存文件

--- runtime/cache/4e/819c837d54a6ed09abc77a8560a66f.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:8:{s:10:"login_init";a:1:{i:0;s:23:"\addons\loginbg\Loginbg";}s:8:"testhook";a:1:{i:0;s:25:"\addons\markdown\Markdown";}s:8:"sms_send";a:2:{i:0;s:19:"\addons\rlsms\Rlsms";i:1;s:21:"\addons\smsbao\Smsbao";}s:10:"sms_notice";a:2:{i:0;s:19:"\addons\rlsms\Rlsms";i:1;s:21:"\addons\smsbao\Smsbao";}s:9:"sms_check";a:2:{i:0;s:19:"\addons\rlsms\Rlsms";i:1;s:21:"\addons\smsbao\Smsbao";}s:12:"upload_after";a:1:{i:0;s:19:"\addons\thumb\Thumb";}s:15:"wipecache_after";a:1:{i:0;s:23:"\addons\tinymce\Tinymce";}s:11:"set_tinymce";a:1:{i:0;s:23:"\addons\tinymce\Tinymce";}}
